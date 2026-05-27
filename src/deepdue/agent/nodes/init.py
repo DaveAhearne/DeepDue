7f21ffc186b5cd21f896e605fa9adb4368964f15
David A'Hearne <david.ahearnex@gmail.com>
2026-05-27 16:51:52 +0100

    Removed zip, dead code and used correct initial state object

--- src/deepdue/agent/nodes/init.py
+++ src/deepdue/agent/nodes/init.py
@@ -1,17 +1,18 @@
-from deepdue.agent.state import InvestigationState
+from deepdue.agent.state import InputState, InvestigationState
 from deepdue.enums import InvestigationEntityType
 from deepdue import models
 
-def node(state: InvestigationState) -> dict:
+def node(state: InputState) -> dict:
     current_node = models.InvestigationEntity(
-        id=state["target_company_number"], 
+        id=state.target_company_number, 
         type=InvestigationEntityType.COMPANY,
         depth=0
     )
 
     return {
-        "current_entity_id": state["target_company_number"], 
-        "entities_to_investigate" : [current_node],
+        "target_company_number": state.target_company_number,
+        "current_entity_id": state.target_company_number, 
         "current_entity_type": InvestigationEntityType.COMPANY,
-        "depth": 0
+        "entities_to_investigate": [current_node],
+        "max_depth": state.max_depth,
     }
